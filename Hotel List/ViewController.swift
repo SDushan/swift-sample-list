4e6c07907853f78a50434446a9618d352a7479ca
SDushan <samithadushan@gmail.com>
2021-08-09 15:45:43 +0530

    Initial Commit

---
+++ Hotel List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hotel List
+//
+//  Created by Dushan on 2021-08-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
