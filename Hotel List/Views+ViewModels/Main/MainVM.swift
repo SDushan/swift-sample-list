e4109f9dcc70a1248208f56f78c85fb6afd0aa84
SDushan <samithadushan@gmail.com>
2021-08-15 13:20:16 +0530

    added dummy data

---
+++ Hotel List/Views+ViewModels/Main/MainVM.swift
@@ -0,0 +1,14 @@
+//
+//  MainVM.swift
+//  Hotel List
+//
+//  Created by Dushan on 2021-08-09.
+//
+
+import UIKit
+
+class MainVM: UIViewController {
+    
+    var data: [Hotel] = [Hotel(id: 1, title: "Down-sized 4thgeneration leverage", description: "Distinctio voluptas ea aliquid consequatur. Rerum cupiditate earum repudiandae non quis a. Rem omnis iste et est repellat sapiente. Dolorum temporibus eos perspiciatis quo laborum unde soluta.", address: "2755 Raul Estate\nWest Ervin, AZ 14265-2763", postcode: "42503-7193", phoneNumber: "387-842-0455x71431", latitude: "-60.964344", longitude: "-12.024244", image: ImageSet(small: "http://lorempixel.com/200/200/cats/1/", medium: "http://lorempixel.com/400/400/cats/1/", large: "http://lorempixel.com/800/800/cats/1/")), Hotel(id: 2, title: "Optional intermediate orchestration", description: "Quia unde officiis ut eum quibusdam. Nihil modi omnis aperiam vitae suscipit. Esse ex voluptatem ratione officiis voluptatem adipisci doloremque. Saepe similique libero et dolor corporis rerum. Ut deleniti provident sed praesentium sit voluptas soluta.", address: "719 Darrick Plains Suite 428\nNorth Christineshire, ID 91341", postcode: "06445-4404", phoneNumber: "103-350-9440x83127", latitude: "-84.165738", longitude: "62.221246", image: ImageSet(small: "http://lorempixel.com/200/200/cats/2/", medium: "http://lorempixel.com/400/400/cats/2/", large: "http://lorempixel.com/800/800/cats/2/")),Hotel(id: 3, title: "Balanced dedicated systemengine", description: "Tempore dolor quibusdam reiciendis. Expedita adipisci voluptatem hic numquam necessitatibus. Aut atque provident nihil earum. Nemo occaecati quas natus quidem quis et. Ut excepturi voluptatum quia ut deserunt ex ad nemo. Itaque magnam animi voluptates voluptate.", address: "839 Bailey Village Apt. 175\nPredovicview, AK 24488", postcode: "39373-5729", phoneNumber: "07192487459", latitude: "82.220837", longitude: "2.920050", image: ImageSet(small: "http://lorempixel.com/200/200/cats/3/", medium: "http://lorempixel.com/400/400/cats/3/", large: "http://lorempixel.com/800/800/cats/3/"))]
+    
+}
