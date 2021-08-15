4bf26ac6137b39ed31390c84e29592b38306b46f
SDushan <samithadushan@gmail.com>
2021-08-15 13:19:39 +0530

    added data models to project

---
+++ Hotel List/Models/Hotel.swift
@@ -0,0 +1,22 @@
+//
+//  Hotel.swift
+//  Hotel List
+//
+//  Created by Dushan on 2021-08-09.
+//
+
+import Foundation
+
+struct Hotel {
+    
+    var id: Int?
+    var title: String?
+    var description: String?
+    var address: String?
+    var postcode: String?
+    var phoneNumber: String?
+    var latitude: String?
+    var longitude: String?
+    var image: ImageSet?
+    
+}
---
+++ Hotel List/Models/ImageSet.swift
@@ -0,0 +1,16 @@
+//
+//  ImageSet.swift
+//  Hotel List
+//
+//  Created by Dushan on 2021-08-09.
+//
+
+import Foundation
+
+struct ImageSet {
+    
+    var small: String?
+    var medium: String?
+    var large: String?
+    
+}
