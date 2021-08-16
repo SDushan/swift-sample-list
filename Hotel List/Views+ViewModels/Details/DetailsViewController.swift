1e4d81eb780b0bd4ed59691979dfbb438688734d
SDushan <samithadushan@gmail.com>
2021-08-16 12:58:31 +0530

    display data in DetailsVC

--- Hotel List/Views+ViewModels/Details/DetailsViewController.swift
+++ Hotel List/Views+ViewModels/Details/DetailsViewController.swift
@@ -11,8 +11,24 @@ class DetailsViewController: UIViewController {
     
     var item = Hotel()
     
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var details: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Change navigation title
+        navigationItem.title = "Details"
+        
+        setupUI()
+    }
+    
+    // Draw UI
+    func setupUI() {
+        image.setImageWithUrl(urlString: item.image?.medium ?? "", placeholderImage: UIImage(named: "placeholder")!)
+        name.text = item.title
+        details.text = item.description
     }
     
 }
